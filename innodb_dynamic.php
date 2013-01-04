5e1219a35d519d3037932ad9e0b9f8c68ebce7ea
kevinlekiller <kevinlekiller@gmail.com>
2013-01-04 17:16:19 -0500

    Update innodb_dynamic.php
    Fixed echo.

--- innodb_dynamic.php
+++ innodb_dynamic.php
@@ -7,16 +7,16 @@ $db = new DB();
 echo "This script will convert only your releases, parts and binaries table back to InnoDB.\n";
 echo "This may take a while depending on how large your tables are. Do not stop this script. It will finish.\n\n";
 
-echo "Converting the binaries table back to InnoDB\n";
+echo "Converting the binaries table to InnoDB with Row_Format Dyamic.\n";
 $db->query("ALTER TABLE binaries ENGINE=InnoDB ROW_FORMAT=DYNAMIC;");
-echo "Finished converting the binaries table back to InnoDB.\n\n";
+echo "Finished converting the binaries table to InnoDB with Row_Format Dyamic.\n\n";
 
-echo "Converting the parts table back to InnoDB\n";
+echo "Converting the parts table to InnoDB with Row_Format Dyamic.\n";
 $db->query("ALTER TABLE parts ENGINE=InnoDB ROW_FORMAT=DYNAMIC;");
 echo "Finished converting the parts table back to InnoDB.\n\n";
 
-echo "Converting the releases table back to InnoDB\n";
+echo "Converting the releases table to InnoDB with Row_Format Dyamic.\n";
 $db->query("ALTER TABLE releases ENGINE=InnoDB ROW_FORMAT=DYNAMIC;");
-echo "Finished converting the releases table back to InnoDB.\n\n";
+echo "Finished converting the releases table to InnoDB with Row_Format Dyamic.\n\n";
 
 ?>
