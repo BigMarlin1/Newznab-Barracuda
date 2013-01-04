8a9655c03c3cb6c8cf0cc02d88b53ba19c4cc6a5
kevinlekiller <kevinlekiller@gmail.com>
2013-01-04 17:18:42 -0500

    Update innodb_dynamic.php
    Fixed Echo.

--- innodb_dynamic.php
+++ innodb_dynamic.php
@@ -4,7 +4,7 @@ require_once("/var/www/newznab/www/lib/framework/db.php");
 
 $db = new DB();
 
-echo "This script will convert only your releases, parts and binaries table back to InnoDB.\n";
+echo "This script will convert only your releases, parts and binaries table to InnoDB with Row_Format Dynamic.\n";
 echo "This may take a while depending on how large your tables are. Do not stop this script. It will finish.\n\n";
 
 echo "Converting the binaries table to InnoDB with Row_Format Dyamic.\n";
