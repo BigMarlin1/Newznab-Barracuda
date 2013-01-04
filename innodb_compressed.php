b1c8d479cea86b0e3437b1d0cdf7d17d4eea188c
kevinlekiller <kevinlekiller@gmail.com>
2013-01-04 17:18:04 -0500

    Update innodb_compressed.php
    Fixed Echo.

--- innodb_compressed.php
+++ innodb_compressed.php
@@ -4,19 +4,19 @@ require_once("/var/www/newznab/www/lib/framework/db.php");
 
 $db = new DB();
 
-echo "This script will convert only your releases, parts and binaries table to Barracuda.\n";
+echo "This script will convert only your releases, parts and binaries table to Innodb with Compressed Row_Format.\n";
 echo "This may take a while depending on how large your tables are. Do not stop this script. It will finish.\n\n";
 
-echo "Converting the binaries table to Barracuda\n";
+echo "Converting the binaries table to InnoDB with Row_Format Compressed.\n";
 $db->query("ALTER TABLE binaries ENGINE=InnoDB ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;");
-echo "Finished converting the binaries table to Barracuda.\n\n";
+echo "Finished converting the binaries table to InnoDB with Row_Format Compressed.\n\n";
 
-echo "Converting the parts table to Barracuda\n";
+echo "Converting the parts table to InnoDB with Row_Format Compressed.\n";
 $db->query("ALTER TABLE parts ENGINE=InnoDB ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;");
-echo "Finished converting the parts table to Barracuda.\n\n";
+echo "Finished converting the parts table to InnoDB with Row_Format Compressed.\n\n";
 
-echo "Converting the releases table to Barracuda\n";
+echo "Converting the releases table to InnoDB with Row_Format Compressed.\n";
 $db->query("ALTER TABLE releases ENGINE=InnoDB ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;");
-echo "Finished converting the releases table to Barracuda.\n\n";
+echo "Finished converting the releases table to InnoDB with Row_Format Compressed.\n\n";
 
 ?>
